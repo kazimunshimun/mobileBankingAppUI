7c0745e8cab307a960cc0e3ef3617944ca5488f7
kazimunshimun <anik1.bd@gmail.com>
2016-06-20 18:49:21 +0600

    screen added ios inital project added

---
+++ iOS/mMobileBanking/mMobileBanking/Controllers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mMobileBanking
+//
+//  Created by Anik on 6/20/16.
+//  Copyright © 2016 mStudoApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
